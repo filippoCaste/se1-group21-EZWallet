e0de9ecd9bd4e5b5733ec7dc60936991ba813516
Ana <s306769@studenti.polito.it>
2023-05-18 13:25:56 +0200

    completing test rquirements

--- code/test/auth.integration.test.js
+++ code/test/auth.integration.test.js
@@ -25,16 +25,35 @@ afterAll(async () => {
 });
 
 describe('register', () => {
+/*     - error 400 is returned if there is already a user with the same username and/or email
+ 
+  //check email account doesnt exist
+  //check username is valid (not yet registered)
+  //check valid password (password criterion)
+            //Error 400 is returned if there is already a user with the same username and/or email
+           //Response `data` Content: A message confirming successful insertion
+  //register the new user and display main screen
+  */
 });
 
 describe("registerAdmin", () => { 
     test('Dummy test, change it', () => {
+      //There is only one admin in each family=> check there is not an admin yet
+      //If there is already an Admin=> error 400
+      //Change role of the user to Admin
         expect(true).toBe(true);
     });
 })
 
 describe('login', () => { 
     test('Dummy test, change it', () => {
+      //check email exists
+      //check password
+      //if all valid=> New object with the created accessToken and refreshToken
+      //log in and display main screen
+
+        //error 400 is returned if the user does not exist
+        //error 400 is returned if the supplied password does not match with the one in the database
         expect(true).toBe(true);
     });
 });
@@ -42,5 +61,10 @@ describe('login', () => {
 describe('logout', () => { 
     test('Dummy test, change it', () => {
         expect(true).toBe(true);
+        //delete the created accessToken and refreshToken from DB
+        // A message confirming successful logout
+        //display logIn screen
     });
 });
+
+//STILL MISSING NEW IMPLEMENTED FUNCTIONS TESTS
--- code/test/controller.integration.test.js
+++ code/test/controller.integration.test.js
@@ -24,72 +24,145 @@ afterAll(async () => {
 
 describe("createCategory", () => { 
     test('Dummy test, change it', () => {
+        /*
+        Check permissions to create category
+        //Show error if no permission 
+        Check category and colour doesnt exist
+        //Show error if already exists
+        // error 401 returned if the specified category does not exist
+        //error 401 is returned if new parameters have invalid values
+        Create and add new category
+        */
         expect(true).toBe(true);
     });
 })
 
 describe("updateCategory", () => { 
     test('Dummy test, change it', () => {
+        //Check permissions to update category
+        //Show error if no permission 
+        //Check new category doesnt exist
+        //Show error if already exists
+        // error 401 returned if the specified category does not exist
+        //error 401 is returned if new parameters have invalid values
+        //Update the new category
         expect(true).toBe(true);
     });
 })
 
 describe("deleteCategory", () => { 
     test('Dummy test, change it', () => {
+        //Check permissions to update category
+        //Show error if no permission 
+        //Delete data of the category
+        //Check the category has been deleted
         expect(true).toBe(true);
     });
 })
 
 describe("getCategories", () => { 
     test('Dummy test, change it', () => {
+        //Check permissions to get category
+        //Show error if no permission 
+        //Check category exists
+        //Error if category doesnt exist
+        //Display category
         expect(true).toBe(true);
     });
 })
 
 describe("createTransaction", () => { 
     test('Dummy test, change it', () => {
+       /*//Check permissions to create Transactions
+        //Show error if no permission 
+        //Check valid attributes : `username`, `type` and `amount`
+        //Check valid attributes `username`, `type`, `amount` and `date`
+        //Show error if not valid attributes(error 401 is returned if the username or the type of category does not exist)
+    */
         expect(true).toBe(true);
     });
 })
 
 describe("getAllTransactions", () => { 
     test('Dummy test, change it', () => {
-        expect(true).toBe(true);
+                expect(true).toBe(true);
+        /*
+        //Check permissions to get Transactions
+        //Show error if no permission 
+        //Show error if no transaction
+        //Display All type transactions
+        
+*/
     });
 })
 
 describe("getTransactionsByUser", () => { 
     test('Dummy test, change it', () => {
         expect(true).toBe(true);
+        //Check permissions to get Transactions
+        //Show error if no permission 
+        //Check selected user transactions exist
+        //Show error if user transactions doesnt exist
+        //Display only user transactions
     });
 })
 
 describe("getTransactionsByUserByCategory", () => { 
     test('Dummy test, change it', () => {
         expect(true).toBe(true);
+        //Check permissions to get Transactions
+        //Show error if no permission 
+        //Check selected category transactions exist
+        //Show error if category transactions doesnt exist
+        //Display only user transactions
     });
 })
 
 describe("getTransactionsByGroup", () => { 
     test('Dummy test, change it', () => {
         expect(true).toBe(true);
+        //Check permissions to get Transactions
+        //Show error if no permission 
+        //Check selected group transactions exist
+        //Show error if group transactions doesnt exist
+        //Display only user transactions
     });
 })
 
 describe("getTransactionsByGroupByCategory", () => { 
     test('Dummy test, change it', () => {
         expect(true).toBe(true);
+        //Check permissions to get Transactions
+        //Show error if no permission 
+        //Check selected category and group transactions exist
+        //Show error if category and group transactions doesnt exist
+        //Display only user transactions
     });
 })
 
 describe("deleteTransaction", () => { 
     test('Dummy test, change it', () => {
         expect(true).toBe(true);
+        //Check permissions to delete Transactions
+        //Show error if no permission
+        //Show error if no transaction
+        //Delete only selected transaction
+
+        
     });
 })
 
 describe("deleteTransactions", () => { 
     test('Dummy test, change it', () => {
         expect(true).toBe(true);
+          //Check permissions to delete Transactions
+        //Show error if no permission
+        //Show error if no transaction
+        //Delete ALL transactions
+
     });
 })
+
+
+
+//STILL MISSING NEW IMPLEMENTED FUNCTIONS TESTS
